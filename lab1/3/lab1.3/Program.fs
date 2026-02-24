c00ac1d1a8ed02feba571fa0df99d41f4ea61c86
GgameMs <136597329+GgameMs@users.noreply.github.com>
2026-02-25 04:40:27 +0500

    Update Program.fs

--- lab1/3/lab1.3/Program.fs
+++ lab1/3/lab1.3/Program.fs
@@ -1,15 +1,20 @@
-﻿open System
+open System
 
 //1.3 Собственные функции операций над списками
 
-let input ()=
+let rec input ()=
     printfn "Введите кол-во эл. списка"
     let n = int(Console.ReadLine())
-    [
-    for i in 1..n do
-        printfn "Введите эл. списка"
-        yield int(Console.ReadLine())
-    ]
+    if n >= 0
+    then
+        [
+        for i in 1..n do
+            printfn "Введите эл. списка"
+            yield int(Console.ReadLine())
+        ]
+    else
+        printfn "Невозможно создать список"
+        input ()
 
 let ListOps () =
     printfn "Выберите операцию:\n1: удаление элемента по индексу\n2: сцепка двух списков\n3: получение элемента по индексу"
@@ -44,10 +49,12 @@ let main _ =
         printfn "Создание списка"
         let List1 = input ()
         let i = input_index ()
-        if i >= 0
+        if i < List1.Length
         then
             let List2 = del_el_index List1 i
             printfn "Новый список, без элемента под индексом %i: %A" i List2
+        else
+            printf "Индекса нет в списке"
     if n = 2
     then
         printfn "Создание списка 1"
@@ -61,8 +68,10 @@ let main _ =
         printfn "Создание списка"
         let List1 = input ()
         let i = input_index ()
-        if i >= 0
+        if i < List1.Length
         then
             let NUM = srch_index List1 i
             printfn "Элемент, под индексом %i: %i" i NUM
+        else
+            printf "Индекса нет в списке"
     0
