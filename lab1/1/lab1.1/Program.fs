55b611d8b4412d31b9012ec1455d1421d6b838ea
unknown <you@example.com>
2026-02-25 03:36:14 +0500

    init

---
+++ lab1/1/lab1.1/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+//1.1 Сформировать список из чисел, противоположный вводимым значениям
+
+let input =
+    printfn "Введите, сколько чисел вы хотите ввести"
+    int(Console.ReadLine())
+
+let list n =
+    [
+        for i in 1..n do
+            printfn "Введите число"
+            let x = int(Console.ReadLine())
+            yield -x
+    ]
+
+[<EntryPoint>]
+let main _ = 
+    let n = input
+    if n < 0
+    then printf "Список невозможно создать"
+    else printfn "Список противоположных элементов: %A" (list n)
+    0
